0d43f25d853c9ed9623e68d0cf1294b871485681
Allen Veach <allen.veach@gmail.com>
2014-05-15 02:26:05 +0000

    bump sensor working

--- src/bump.py
+++ src/bump.py
@@ -6,7 +6,7 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
 
 class BumpSensor(object):
-    def __init__(self, black_pin = "P8_20", tan_pin = "P8_22"):
+    def __init__(self, black_pin = "P8_7", tan_pin = "P8_26"):
 
         self.black_pin = black_pin
         self.tan_pin = tan_pin
@@ -14,11 +14,13 @@ class BumpSensor(object):
         GPIO.setup(self.black_pin, GPIO.IN)
         GPIO.setup(self.tan_pin, GPIO.IN)
 
-    def didBump():
+    def didBump(self):
         old_switch_state = 0
         while True:
-            new_switch_state = GPIO.input(self.black_pin) or GPIO.input(self.tan_pin)
-            if new_switch_state == 1:
+            time.sleep(0.5)
+            new_switch_state = GPIO.input(self.black_pin) and GPIO.input(self.tan_pin)
+            print new_switch_state
+            if new_switch_state == 0:
                 print 'We have bumped!'
                 return 1
             else:
