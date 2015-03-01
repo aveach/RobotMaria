fcd137dbe58a3bb19a9c27f8c06a7c598ab06c0b
Demo User <debian@beaglebone.localdomain>
2015-03-01 20:49:00 +0000

    - Modified rover.py to use correct serial port names for motor controllers. - Added manual control using keyboard to rover.py

--- src/rover.py
+++ src/rover.py
@@ -4,8 +4,8 @@ import time
 
 class Rover():
     def __init__(self):
-        self.m1 = motor.MotorController('/dev/tty.usbmodem14111')
-        self.m2 = motor.MotorController('/dev/tty.usbmodem14121')
+        self.m1 = motor.MotorController('/dev/ttyACM0')
+        self.m2 = motor.MotorController('/dev/ttyACM1')
         self.Reset()
         self.speed = 0
         return
@@ -45,3 +45,53 @@ class Rover():
         self.m2.exitSafeStart()
         self.Stop()
         return
+
+if __name__ == "__main__":
+	import curses
+	
+	#Initialize Motors
+	rover = Rover()
+	speed = 1000
+	# get the curses screen window
+	screen = curses.initscr()
+
+	# turn off input echoing
+	curses.noecho()
+
+	# respond to keys immediately (don't wait for enter)
+	curses.cbreak()
+
+	# map arrow keys to special values
+	screen.keypad(True)
+
+	try:
+   		while True:
+			char = screen.getch()
+			if char == ord('q'):
+				break
+			elif char == curses.KEY_RIGHT:
+				# print doesn't work with curses, use addstr instead
+				screen.addstr(0, 0, 'right')
+				rover.RotateRight(speed)
+			elif char == curses.KEY_LEFT:
+				screen.addstr(0, 0, 'left ')
+				rover.RotateLeft(speed)
+			elif char == curses.KEY_UP:
+				screen.addstr(0, 0, 'up   ')
+				rover.Forward(speed)
+			elif char == curses.KEY_DOWN:
+				screen.addstr(0, 0, 'down ')
+				rover.Reverse(speed)
+			elif char == ord('>'):
+				speed +=100
+				screen.addstr(0,0, 'speed '+str(speed))
+			elif char == ord('<'):
+				speed -=100
+				screen.addstr(0,0, 'speed '+str(speed))
+			else:
+				screen.addstr(0,0, 'stop ')
+				rover.Stop()
+	finally:
+    	# shut down cleanly
+		curses.nocbreak(); screen.keypad(0); curses.echo()
+		curses.endwin()	
