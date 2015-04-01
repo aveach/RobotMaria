8d67513eadc09a6a378afd1dbedffe23a27bb073
Jose Madrid <jpmadrid@gmail.com>
2015-03-31 20:35:38 -0400

    Added initial code for GPS waypoint tracking

--- src/robot_maria_main.py
+++ src/robot_maria_main.py
@@ -7,6 +7,7 @@ import pixyTracker
 import time
 import signal
 
+
 # Ctrl-c handler
 def ctrlC(signal, frame):
     PixySensor.quit()
@@ -16,27 +17,51 @@ signal.signal(signal.SIGINT,ctrlC)
 
 # Initialize rover
 Rover = rover.Rover()
-RotateSpeed = 2500
+RotateSpeed = 3200
 
 
-# Initialize Pixi
+# Initialize Pixy
 PixySensor = pixyTracker.pixyController('pixy')
 PixySensor.start(None)
 
-while 1:
-    time.sleep(0.1) # We will get data from pixy every 100 ms
+# Initialize WayPoint Sensr (GPS, COMPAS)
+WayPointSensor = sensorState.WaypointSensor("WaypointSensor")
+WayPointSensor.start() 
+HeadingDiff = 7
 
-    #Get data from pixy and rotate till we are facing the cone.
-    ConeAngle = PixySensor.getReading()
-    print "Cone Angle:"+str(ConeAngle)
-    if ConeAngle.value > 2:
-        Rover.RotateRight(RotateSpeed)
-        print "Rotate Right"
-    elif ConeAngle.value < -2:
-        print "Rotate Left"
-        Rover.RotateLeft(RotateSpeed)
-    else:
-        print "We found the Cone !!! Pixy Residual Angle:"+str(ConeAngle)
-        Rover.Stop()
+# Initia; State
+MachineState = "WayPoint"
+
+while True:
+    time.sleep(0.1) # We will get data every 100 ms
+
+    if MachineState == "Pixy":
+        #Get data from pixy and rotate till we are facing the cone.
+        ConeAngle = PixySensor.getReading()
+        print "Cone Angle:"+str(ConeAngle)
+        if ConeAngle.value > 2:
+            Rover.RotateRight(RotateSpeed)
+            print "Rotate Right"
+        elif ConeAngle.value < -2:
+            print "Rotate Left"
+            Rover.RotateLeft(RotateSpeed)
+        else:
+            Rover.Forward(RotateSpeed)
+            #print "We found the Cone !!! Pixy Residual Angle:"+str(ConeAngle)
+            Rover.Stop()
     
 
+    if MachineState == "WayPoint":
+       WayPointData = WayPointSensor.getReading().value
+       if WayPointData["distanceToNextWaypoint"] <= 12:
+           print "We are close to cone, switch to Pixy. Distance: "+str(WayPointData["distanceToNextWaypoint"]) 
+       else:
+           if WayPointData["currentHorizontalAccuracy"] < 20:
+               AngleDiff = WayPointData["headingToNextWaypoint"] - WayPointData["direction"]
+               if (AngleDiff < -180):
+                   AngleDiff += 360
+               elif (AngleDiff > 180):
+                   AngleDiff -= 360
+               
+               # Allow a small degree of diviation before taking action
+               if abs( 
